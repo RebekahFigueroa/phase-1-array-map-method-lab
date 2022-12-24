5ff79f057c5f8f18ceac8fae9d0fc6f70833a3b1
Rebekah <figueroarebekah@gmail.com>
2022-12-23 22:01:48 -0600

    commit tree 46aa2ea2abff3daa3206bf858cb67c7912c849e9

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((element) => {
+    const words = element.split(" ");
+    const arrayCapitalWords = words.map(
+      (wordElement) =>
+        wordElement.charAt(0).toUpperCase() + wordElement.slice(1)
+    );
+    return arrayCapitalWords.join(" ");
+  });
+};
